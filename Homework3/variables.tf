2d7d2d60285c553f543772df620743d9be0e55ed
rotemad <rotem@MacBook-Pro.local>
2020-11-12 17:59:58 +0200

    update chnages due to misunderstood of the instructions

--- Homework3/variables.tf
+++ Homework3/variables.tf
@@ -10,10 +10,11 @@ variable "user_data" {
   default = <<-EOF
               #!/bin/bash
               sudo apt-get update
-              sudo apt-get install nginx -y
+              sudo apt-get install awscli nginx -y
               instanceId=$(curl http://169.254.169.254/latest/meta-data/local-hostname) 
               echo $instanceId > /var/www/html/index.nginx-debian.html 
               sudo systemctl start nginx.service
               sudo systemctl enable nginx.service
+              (crontab -l 2>/dev/null; echo " @hourly awscli s3 cp /var/log/nginx/access.log s3://web-logs/$instanceId-log.txt") | crontab -
               EOF
 }
